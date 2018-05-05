b8c4e5f77dd81ac39b9ea6165e54faa79197c137
Ralf Walk <ralf.walk@allianz.de>
2018-05-05 20:22:24 +0200

    added home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {WishlistService} from '../services/wishlist.service';
+import {Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private sub: Subscription;
+
+  constructor(private wishlistService: WishlistService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.sub = this.wishlistService.getRootUpdates().subscribe((wishlist) => {
+      if (wishlist) {
+        const path = wishlist.id + '-' + wishlist.password;
+        this.router.navigate([path]);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  createWishlist(wishlistInfo) {
+    if (wishlistInfo) {
+      this.wishlistService.createWishlist(wishlistInfo);
+    }
+  }
+}
